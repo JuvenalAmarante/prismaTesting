593961e5b2a63d7c96b185d545109047ba2f255f
Juvenal Amarante <juvenalamarante13@gmail.com>
2023-03-29 15:11:14 -0300

    feat: add class-validator and class-transformer

--- src/User/dto/create-user.dto.ts
+++ src/User/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsInt()
+  @IsNotEmpty()
   id: number;
+
+  @IsString()
+  @IsOptional()
   name: string | null;
+
+  @IsString()
+  @IsNotEmpty()
   email: string;
 }
